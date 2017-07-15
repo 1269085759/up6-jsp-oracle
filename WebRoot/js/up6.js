437104bf69a43f783c6db18cf6a4eb71601c95ab
qwl <qwl@ncmem.com>
2017-07-15 14:19:22 +0800

    更新版本号

--- WebRoot/js/up6.js
+++ WebRoot/js/up6.js
@@ -86,7 +86,7 @@ function HttpUploaderMgr()
 	this.Config = {
 		  "EncodeType"		: "utf-8"
 		, "Company"			: "荆门泽优软件有限公司"
-		, "Version"			: "2,7,107,51160"
+		, "Version"			: "2,7,108,51163"
 		, "License"			: ""//
 		, "Authenticate"	: ""//域验证方式：basic,ntlm
 		, "AuthName"		: ""//域帐号
